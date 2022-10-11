f73cebd73dd1d5e7e472c3998ab4257a5467b93a
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2022-10-11 16:32:38 -0500

    Move compiled plugins to `bin/plugins`

--- build.sh
+++ build.sh
@@ -1,15 +1,16 @@
-rm -rf dll/*.prx
+rm -rf bin/plugins/*.prx
 
 echo "#define GIT_COMMIT \"$(git rev-parse HEAD)\"" > common/git_ver.h
 echo "#define GIT_VER \"$(git branch --show-current)_1.$(git rev-list HEAD --count)\"" >> common/git_ver.h
 echo "#define GIT_NUM $(git rev-list HEAD --count)" >> common/git_ver.h
 echo "#define BUILD_DATE \"$(date '+%b %d %Y @ %T (%Z %z)')\"" >> common/git_ver.h
 
+FINAL="FINAL=-DFINAL"
+
 if [ $1 -gt "0" ]
 then
     FINAL="FINAL="
 fi
 
-FINAL="FINAL=-DFINAL"
 cd plugin-src
 for dir in ./*; do (cd "$dir" && make clean && make $FINAL); done
