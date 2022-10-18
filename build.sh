5013225de15711e7f057603de9e8ce95f143829e
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2022-10-18 16:49:49 -0500

    Add original binary to artifact

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-rm -rf bin/plugins/*.prx
+rm -rf bin/plugins/*.prx bin/plugins/*.elf
 
 echo "#define GIT_COMMIT \"$(git rev-parse HEAD)\"" > common/git_ver.h
 echo "#define GIT_VER \"$(git branch --show-current)_1.$(git rev-list HEAD --count)\"" >> common/git_ver.h
