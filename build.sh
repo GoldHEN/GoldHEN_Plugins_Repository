c282d9f6e7da617e328752827e05d7f3071698d4
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2022-10-21 23:43:07 -0500

    Remove branch version

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 rm -rf bin/plugins/*.prx bin/plugins/*.elf
 
 echo "#define GIT_COMMIT \"$(git rev-parse HEAD)\"" > common/git_ver.h
-echo "#define GIT_VER \"$(git branch --show-current)_1.$(git rev-list HEAD --count)\"" >> common/git_ver.h
+echo "#define GIT_VER \"$(git branch --show-current)\"" >> common/git_ver.h
 echo "#define GIT_NUM $(git rev-list HEAD --count)" >> common/git_ver.h
 echo "#define BUILD_DATE \"$(date '+%b %d %Y @ %T (%Z %z)')\"" >> common/git_ver.h
 
