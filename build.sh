cf514ec391e00893917674e2bcf67f740e9a0c5a
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2022-10-22 11:31:45 -0500

    Simplify CI build steps and generate hash for modules

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-rm -rf bin/plugins/*.prx bin/plugins/*.elf
+rm -rf bin/plugins/**/*.prx bin/plugins/**/*.elf
 
 echo "#define GIT_COMMIT \"$(git rev-parse HEAD)\"" > common/git_ver.h
 echo "#define GIT_VER \"$(git branch --show-current)\"" >> common/git_ver.h
@@ -13,4 +13,25 @@ then
 fi
 
 cd plugin-src
-for dir in ./*; do (echo "$dir" && cd "$dir" && make clean && make $FINAL); done
+for dir in ./*
+  do
+    echo "$dir"
+    cd "$dir"
+    make clean
+    make $FINAL || exit 1
+    cd ..
+  done
+
+cd ..
+
+BUILD=bin/plugins/prx
+touch $BUILD/md5.txt $BUILD/sha256.txt
+echo "MD5:" > $BUILD/md5.txt
+echo "SHA256:" > $BUILD/sha256.txt
+FILES="$BUILD/*.prx"
+for f in $FILES
+  do
+    echo "$(sha256sum $f)" >> $BUILD/sha256.txt
+    echo "$(md5sum $f)" >> $BUILD/md5.txt
+  done
+cat $BUILD/sha256.txt $BUILD/md5.txt
