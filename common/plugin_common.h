98d25487915a357cc32815a1d1897af8a370b0bb
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2022-10-23 11:44:32 -0500

    Minor spacing and no redundant types

--- common/plugin_common.h
+++ common/plugin_common.h
@@ -10,25 +10,25 @@
 #define s64 int64_t
 
 #ifdef FINAL
-#define BUILD_TYPE "Final (Release)"
+#define BUILD_TYPE "(Release)"
 #define debug_printf(a, args...)
 #else
-#define BUILD_TYPE "Not Final (Debug)"
+#define BUILD_TYPE "(Debug)"
 #define debug_printf(a, args...) printf("[%s] (%s:%d) " a,  __func__,__FILE__, __LINE__, ##args)
 #endif
 #define final_printf(a, args...) printf("(%s:%d) " a, __FILE__, __LINE__, ##args)
 #define boot_ver() {\
-  final_printf("Commit: %s Branch: %s Build: %i Type: %s\n", GIT_COMMIT, GIT_VER, GIT_NUM, BUILD_TYPE);\
+  final_printf("Commit: %s Branch: %s Build: %i %s\n", GIT_COMMIT, GIT_VER, GIT_NUM, BUILD_TYPE);\
   final_printf("Built: %s\n", BUILD_DATE);\
 }
 
 #define print_proc_info() {\
   final_printf("process info\n");\
-  final_printf("pid:%d\n", procInfo.pid);\
-  final_printf("name:%s\n", procInfo.name);\
-  final_printf("path:%s\n", procInfo.path);\
-  final_printf("titleid:%s\n", procInfo.titleid);\
-  final_printf("contentid:%s\n", procInfo.contentid);\
-  final_printf("version:%s\n", procInfo.version);\
-  final_printf("base_address:0x%lx\n", procInfo.base_address);\
+  final_printf("pid: %d\n", procInfo.pid);\
+  final_printf("name: %s\n", procInfo.name);\
+  final_printf("path: %s\n", procInfo.path);\
+  final_printf("titleid: %s\n", procInfo.titleid);\
+  final_printf("contentid: %s\n", procInfo.contentid);\
+  final_printf("version: %s\n", procInfo.version);\
+  final_printf("base_address: 0x%lx\n", procInfo.base_address);\
 }
