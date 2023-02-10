822a53d6f1cfcbb9d8bfa3b98b22932f02af7d9f
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2023-02-10 15:47:46 -0600

    Game Patch: move to cpp for constexpr

--- plugin_src/game_patch/include/patch.h
+++ plugin_src/game_patch/include/patch.h
@@ -11,7 +11,16 @@ extern char game_ver[8];
 unsigned char *hexstrtochar2(const char *hexstr, s64 *size);
 void sys_proc_rw(u64 address, void *data, u64 length);
 bool hex_prefix(const char *str);
-u64 djb2(const char *str);
+
+// http://www.cse.yorku.ca/~oz/hash.html
+constexpr inline u64 djb2_hash(const char *str) {
+    u64 hash = 5381;
+    u32 c = 0;
+    while ((c = *str++))
+        hash = hash * 33 ^ c;
+    return hash;
+}
+
 u64 patch_hash_calc(const char *title, const char *name, const char *app_ver,
                     const char *title_id, const char *elf);
-void patch_data1(const char *type, u64 addr, const char *value);
+void patch_data1(u64 patch_type, u64 addr, const char *value);
