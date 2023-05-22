fc4ac6a8954a6789423c5c6e0e0d45990ad6ecd9
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2023-05-22 07:44:03 -0500

    Game Patch: Remove unused parameter

--- plugin_src/game_patch/include/patch.h
+++ plugin_src/game_patch/include/patch.h
@@ -2,7 +2,7 @@
 #include "plugin_common.h"
 #include <stdbool.h>
 
-extern s32 pid;
+#pragma once
 
 extern char titleid[16];
 extern char game_elf[32];
@@ -13,14 +13,8 @@ void sys_proc_rw(u64 address, void *data, u64 length);
 bool hex_prefix(const char *str);
 
 // http://www.cse.yorku.ca/~oz/hash.html
-constexpr inline u64 djb2_hash(const char *str) {
-    u64 hash = 5381;
-    u32 c = 0;
-    while ((c = *str++))
-        hash = hash * 33 ^ c;
-    return hash;
-}
+constexpr inline u64 djb2_hash(const char *str);
 
 u64 patch_hash_calc(const char *title, const char *name, const char *app_ver,
                     const char *title_id, const char *elf);
-void patch_data1(u64 patch_type, u64 addr, const char *value, const char *mask_offset, uint32_t source_size, uint64_t jump_target);
+void patch_data1(const char* patch_type_str, u64 addr, const char *value, uint32_t source_size, uint64_t jump_target);
