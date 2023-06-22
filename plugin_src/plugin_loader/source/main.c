6995e71354ff66720253fe12eaa3e9ddee844887
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2023-06-21 23:59:30 -0500

    Update main.c

--- plugin_src/plugin_loader/source/main.c
+++ plugin_src/plugin_loader/source/main.c
@@ -136,7 +136,10 @@ void load_plugins(ini_section_s *section, uint32_t *load_count)
             if (ModuleName)
             {
                 *load_count += 1;
-                snprintf(PluginDetails, sizeof(PluginDetails), "%s%u. %s\n", PluginDetails, *load_count, *ModuleName);
+                char plugin_entry[128];
+                snprintf(plugin_entry, sizeof(plugin_entry), "%u. %s\n", *load_count, *ModuleName);
+                strcat(PluginDetails, plugin_entry);
+                //snprintf(PluginDetails, sizeof(PluginDetails), "%s");
             }
         }
     }
